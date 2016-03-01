bc69faac6a6d462c26803e411c1e58c547c5c8dd
Tinkerlad0 <tinkerlad0@gmail.com>
2016-03-02 10:36:07 +1100

    Initial Commit

---
+++ src/week1/DayOfWeek.java
@@ -0,0 +1,21 @@
+package week1;
+
+/**
+ *
+ * This program is designed to take user input to select a date.
+ * It will then tell you the day of the week that the date falls on.
+ *
+ * @author michael  1/03/16.
+ * @version 0.0.1
+ */
+public class DayOfWeek {
+
+    /**
+     * Entry point for regular program operation.
+     * There is no need for any arguments
+     * @param args the cli arguments.
+     */
+    public static void main(String[] args) {
+
+    }
+}
